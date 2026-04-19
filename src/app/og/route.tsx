de35125bd82e14e139751b4c938cfcbcf020b11b
pokhrelRaman <ramanpokh@gmail.com>
2026-04-20 01:09:47 +0545

    og fix

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
